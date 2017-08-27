796601f882ce39294626fbe28d15a97e25beef44
wenlong <simbawwl@gmail.com>
2017-08-27 09:39:28 +0800

    acceptance pending

---
+++ accept/lla.sh
@@ -0,0 +1,10 @@
+latex    lla.tex
+bibtex   lla
+latex    lla.tex
+latex    lla.tex
+latex    lla.tex
+dvips    lla
+ps2pdf   lla.ps
+#pdffonts lla.pdf
+#evince   lla.g.pdf &
+rm *.lot *.aux *.bbl *.blg *.dvi *.lof *.log *.ps *.nav *.out *.snm *.toc
